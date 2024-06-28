70616f18d8e7105696569c15c9b794f119f89f95
efzr <emerson.zapatarivas@gmail.com>
2024-06-27 23:31:52 -0600

    feature: authentication styles applied correctly

---
+++ app/authentication/login/page.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import "@/app/styles/authentication/login.css";
+
+export default function page() {
+  return (
+    <section className="login section container grid">
+      <h1 className="login__title">
+        ¡Hola, Bienvenido de nuevo!
+      </h1>
+      <span className="login__subtitle">
+        ¿Nuevo en Crowdfunding?{" "}
+        <Link href="/authentication/register">¡Regístrate!</Link>
+      </span>
+
+      <div className="login__mode grid">
+        <div className="login__button-container grid">
+          <button className="login__oauth-button">
+            <Image
+              src={"/google.svg"}
+              width={30}
+              height={30}
+              alt="Google OAuth 2.0"
+            />
+            <span>Continúa con Google</span>
+          </button>
+          <button className="login__oauth-button">
+            <Image
+              src={"/facebook.svg"}
+              width={30}
+              height={30}
+              alt="Facebook OAuth 2.0"
+            />
+            <span>Continúa con Facebook</span>
+          </button>
+          <button className="login__oauth-button">
+            <Image
+              src={"/apple.svg"}
+              width={30}
+              height={30}
+              alt="Apple OAuth 2.0"
+            />
+            <span>Continúa con Apple</span>
+          </button>
+        </div>
+
+        <div>
+          <form className="login__form grid">
+            <div className="field">
+              <input type="email" id="email" name="email" placeholder="" />
+              <label className="label" htmlFor="email">
+                Correo Electronico
+              </label>
+            </div>
+
+            <div className="field">
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                name="password"
+              />
+              <label className="label" htmlFor="password">
+                Contraseña
+              </label>
+            </div>
+
+            <input type="submit" className="button" />
+          </form>
+        </div>
+      </div>
+
+      {/* TODO: Implement email auth to make it work */}
+      <Link href="" className="forgot__password-link">
+        ¿Olvidaste tu Contraseña?
+      </Link>
+    </section>
+  );
+}
---
+++ app/authentication/register/page.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import "@/app/styles/authentication/register.css";
+
+export default function page() {
+  return (
+    <section className="register section container grid">
+      <h1 className="register__title">
+        ¡Invierte en los fundadores en los que confías!
+      </h1>
+      <span className="register__subtitle">
+        ¿Ya tienes una cuenta en Crowdfunding?{" "}
+        <Link href="/authentication/login">¡Inicia Sesión!</Link>
+      </span>
+
+      <div className="register__mode grid">
+        <div className="register__button-container grid">
+          <button className="register__oauth-button">
+            <Image
+              src={"/google.svg"}
+              width={30}
+              height={30}
+              alt="Google OAuth 2.0"
+            />
+            <span>Continúa con Google</span>
+          </button>
+          <button className="register__oauth-button">
+            <Image
+              src={"/facebook.svg"}
+              width={30}
+              height={30}
+              alt="Google OAuth 2.0"
+            />
+            <span>Continúa con Facebook</span>
+          </button>
+          <button className="register__oauth-button">
+            <Image
+              src={"/apple.svg"}
+              width={30}
+              height={30}
+              alt="Google OAuth 2.0"
+            />
+            <span>Continúa con Apple</span>
+          </button>
+        </div>
+
+        <form className="register__form grid">
+          <div className="field">
+            <input type="email" id="email" name="email" placeholder="" />
+            <label className="label" htmlFor="email">
+              Correo Electrónico
+            </label>
+          </div>
+
+          <div className="field">
+            <input type="text" id="name" placeholder="" name="name" />
+            <label className="label" htmlFor="name">
+              Nombre Completo
+            </label>
+          </div>
+
+          <div className="field">
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              name="password"
+            />
+            <label className="label" htmlFor="password">
+              Contraseña
+            </label>
+          </div>
+
+          <input type="submit" className="button" />
+        </form>
+      </div>
+    </section>
+  );
+}
