eb0b5b86af8135094415199b42a7e0743750b0e8
efzr <emerson.zapatarivas@gmail.com>
2024-07-20 19:42:31 -0600

    Middleware working succesfully

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
   const { nextUrl, cookies } = req;
-  const token = cookies.get("set-cookie")?.value;
+  const token = cookies.get("token")?.value;
+  console.log(nextUrl.pathname);
 
-  if (token && ["authentication"].includes(nextUrl.pathname)) {
-    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}`);
+  if (
+    token &&
+    ["/authentication/login", "/authentication/register"].includes(
+      nextUrl.pathname
+    )
+  ) {
+    return NextResponse.redirect(
+      new URL(`${process.env.NEXT_PUBLIC_SERVER_URL}`, req.url)
+    );
   }
 
   return NextResponse.next();
